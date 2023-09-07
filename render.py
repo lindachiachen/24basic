69049dc3b95b21376512d886863c3d2f21daf2ab
Ian Chen <ianchen06@gmail.com>
2023-09-07 11:50:30 -0700

    test

--- render.py
+++ render.py
@@ -7,11 +7,19 @@ TEMPLATE_DIR = "./templates"
 OUTPUT_DIR = "./dist"
 BASE_DIR = os.getcwd()
 
-env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader("templates"),
+    trim_blocks=True,
+    lstrip_blocks=True,
+)
 tmpls = [x for x in os.walk(TEMPLATE_DIR)][0][2]
 for tmpl in tmpls:
     template = env.get_template(tmpl)
     with open(os.path.join(OUTPUT_DIR, tmpl), "w") as f:
         f.write(template.render())
 
-shutil.copytree(os.path.join(BASE_DIR, "assets"), os.path.join(OUTPUT_DIR, "static"), dirs_exist_ok=True)
+shutil.copytree(
+    os.path.join(BASE_DIR, "assets"),
+    os.path.join(OUTPUT_DIR, "static"),
+    dirs_exist_ok=True,
+)
